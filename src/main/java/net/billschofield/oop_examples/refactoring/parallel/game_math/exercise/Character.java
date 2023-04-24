c15ee00bbe43ea2241d9c69f5aa8b1eed1dd3c89
Bill Schofield <bschofield@yahoo.com>
2023-04-24 16:45:32 -0500

    Added exercise package to do the actual live refactoring in.

---
+++ src/main/java/net/billschofield/oop_examples/refactoring/parallel/game_math/exercise/Character.java
@@ -0,0 +1,45 @@
+package net.billschofield.oop_examples.refactoring.parallel.game_math.exercise;
+
+import static java.lang.Math.PI;
+
+public class Character {
+	private final String name;
+	private double heading;
+	private double x;
+	private double y;
+
+	public Character(String name, double heading, double x, double y) {
+		this.name = name;
+		this.heading = heading;
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setHeading(double heading) {
+		this.heading = heading;
+	}
+
+	public double getHeading() {
+		return heading;
+	}
+
+	public String toString(){
+		return "Character: " + name + ", Heading: " + heading * 180/ PI + ",  Position: (" + x + ", " + y + ")";
+	}
+}
