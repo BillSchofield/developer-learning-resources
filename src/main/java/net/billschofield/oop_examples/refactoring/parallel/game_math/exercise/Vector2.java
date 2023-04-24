b9c26710237df06ae1e1ae781bf4c81396c2b06e
Bill Schofield <bschofield@yahoo.com>
2023-04-24 16:47:46 -0500

    Create Vector2.java
    Added trivial Vector2 class to exercise.

---
+++ src/main/java/net/billschofield/oop_examples/refactoring/parallel/game_math/exercise/Vector2.java
@@ -0,0 +1,19 @@
+package net.billschofield.oop_examples.refactoring.parallel.game_math.exercise;
+
+public class Vector2 {
+	private final double x;
+	private final double y;
+
+	public Vector2(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+}
